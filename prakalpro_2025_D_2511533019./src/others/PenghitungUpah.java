5e81517125b7fff075ffa43b57b58b5c94bd557f
Muhammad Althaf Mulya <althafmain@gmail.com>
2025-09-28 22:33:26 +0700

    menambahkan penghitungan upah

--- prakalpro_2025_D_2511533019./src/others/PenghitungUpah.java
+++ prakalpro_2025_D_2511533019./src/others/PenghitungUpah.java
@@ -11,17 +11,6 @@ public class PenghitungUpah {
     public static void main(String[] args) {
         console.log("Memulai Program");
 
-        // Golongan
-        String GolonganA = "A";
-        String GolonganB = "B";
-        String GolonganC = "C";
-        String GolonganD = "D";
-
-        // Gaji karyawan pergolongan
-        int gajiA = 1000;
-        int gajiB = 2000;
-        int gajiC = 3000;
-        int gajiD = 4000;
 
         // Input nama karyawan
         Scanner input = new Scanner(System.in);
@@ -35,18 +24,41 @@ public class PenghitungUpah {
         // Input jam kerja karyawan
         System.out.print("Masukan jam kerja: ");
         int jamKerja = input.nextInt();
+        input.close();
 
         console.log("Nama: " + nama);
         console.log("Golongan: " + golongan);
         console.log("Jam Kerja: " + jamKerja + " jam perminggu");
 
 
-        if (golongan == GolonganA) {
-            console.log(
-                nama + "dengan golongan" + golongan + jamKerja + "jam perminggu"
-            );
+        int upahPerJam = 0;
+
+        if (golongan.equalsIgnoreCase("A")) {
+            upahPerJam = 1000;
+        } else if (golongan.equalsIgnoreCase("B")) {
+            upahPerJam = 2000;
+        } else if (golongan.equalsIgnoreCase("C")) {
+            upahPerJam = 3000;
+        } else if (golongan.equalsIgnoreCase("D")) {
+            upahPerJam = 4000;
         }
 
+        // Hitung gaji karyawan
+        int gaji = jamKerja * upahPerJam;
+
+        // Hitung lembur jika > 60
+        if (jamKerja > 60) {
+            int isLembur = (jamKerja - 60) * 5000;
+            gaji += isLembur;
+        }
+
+        // Tampilkan ke layar
+        console.log("//================================//");
+        console.log("Nama Karyawan      : " + nama);
+        console.log("Golongan           : " + golongan);
+        console.log("Jam Kerja          : " + jamKerja + "jam");
+        console.log("Upah perminggu     : " + gaji);
+        console.log("//================================//");
     };
 
 }
