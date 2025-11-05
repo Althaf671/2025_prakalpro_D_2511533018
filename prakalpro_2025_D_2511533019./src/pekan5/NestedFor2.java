4f7fa0d98596abb68bf3a6c7e24b5bede2a52634
althaf <althafmain@gmail.com>
2025-11-05 14:50:01 +0700

    pekan 6

--- prakalpro_2025_D_2511533019./src/pekan5/NestedFor2.java
+++ prakalpro_2025_D_2511533019./src/pekan5/NestedFor2.java
@@ -4,17 +4,16 @@ import shared.MyLogger;
 
 public class NestedFor2 {
 
-	static MyLogger ada = new MyLogger();
+	static MyLogger Ada = new MyLogger();
 	
 	public static void main(String[] args) {
 		
 		for (int i = 0; i <= 5; i++) {
 			for (int j = 0; j <= 5; j++) {
-				ada.Text_IO_Put(i + j + " ");
+				Ada.Text_IO_Put(i + j + " ");
 			}
-			ada.Text_IO_Put_Line("");
+			Ada.Text_IO_Put_Line("");
 		}
 
 	}
-
 }
---
+++ prakalpro_2025_D_2511533019./src/pekan6_2511533018/perulanganWhile1_2511533018.java
@@ -0,0 +1,29 @@
+package pekan6_2511533018;
+
+import java.util.Scanner;
+
+public class perulanganWhile1_2511533018 {
+
+	public static void main(String[] args) {
+		
+		int counter = 0;
+		String jawab;
+		boolean running = true;
+		Scanner scan = new Scanner(System.in);
+		
+		while (running) {
+			counter++;
+			System.out.println("Jumlah = " + counter);
+			System.out.print("Apakah lanjut (ya/tidak?)");
+			jawab = scan.nextLine();
+			
+			// Cek jawab = tidak, perulangan berhenti.
+			if (jawab.equalsIgnoreCase("tidak")) {
+				running = false;
+			}
+		}
+		System.out.println("Anda sudah melakukan perulangan sebanyak " + counter + " kali");
+
+	}
+
+}
